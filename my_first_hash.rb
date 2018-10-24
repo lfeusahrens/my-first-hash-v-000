d27d04d4e2f296d2f184c49ef379371ea66503a2
Lauren Feusahrens <lrfeusahrens@gmail.com>
2018-10-24 19:53:46 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -34,12 +34,10 @@ def adding
   shipping_manifest = {
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
-                  "oil paintings" => 3
+                  "oil paintings" => 3,
+                  "muskets" => 2,
+                  "gun powder" => 4
                  }
-  
-  shipping_manifest["muskets"] = 2
-
-  shipping_manifest["gun powder"] = 4
 
   puts shipping_manifest
 end
